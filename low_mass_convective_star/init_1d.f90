493bf8e208a74d06c9b547eaabc001850a780ebd
Michael Zingale <michael.zingale@stonybrook.edu>
2020-02-05 14:01:46 -0500

    this seems to work

--- low_mass_convective_star/init_1d.f90
+++ low_mass_convective_star/init_1d.f90
@@ -1,4 +1,4 @@
-!! generate an initial model for spherical geometry with a 
+!! generate an initial model for spherical geometry with a
 !! uniform composition.  Here we take a base density and temperature
 !! and use HSE and constant entropy to generate the model.
 
@@ -17,9 +17,8 @@ program init_1d
   integer :: i, n
 
   integer :: nx
-      
-  real (kind=dp_t) :: temp_base, dens_base
-  real (kind=dp_t), DIMENSION(nspec) :: xn_base
+
+  real (kind=dp_t), DIMENSION(nspec) :: xn_core
 
   real (kind=dp_t), allocatable :: xzn_hse(:), xznl(:), xznr(:)
   real (kind=dp_t), allocatable :: model_hse(:,:), M_enclosed(:)
@@ -36,8 +35,8 @@ program init_1d
   real (kind=dp_t), parameter :: M_sun = 1.9891e33
 
   ! we'll get the composition indices from the network module
-  integer, save :: ic12, io16, img24, iash
-  real (kind=dp_t) :: cfrac
+  integer, save :: ih1, ihe4
+  real (kind=dp_t) :: hefrac
 
   integer :: narg
   character(len=128) :: params_file
@@ -56,13 +55,16 @@ program init_1d
 
   integer, parameter :: MAX_ITER = 250
 
-  integer :: iter
+  integer :: iter, iter_mass
 
-  logical :: converged_hse, fluff
+  logical :: converged_hse, mass_converged, fluff
+
+  real (kind=dp_t) :: mass_star, mass_star_old
 
   real (kind=dp_t), dimension(nspec) :: xn
 
-  real (kind=dp_t), save :: low_density_cutoff, temp_fluff, smallx, dens_conv_zone, M_conv_zone
+  real (kind=dp_t), save :: low_density_cutoff, temp_core, smallx, M_tot, temp_fluff
+  real (kind=dp_t) :: rho_c, rho_c_old, drho_c
 
   logical :: isentropic
 
@@ -71,15 +73,17 @@ program init_1d
 
   real (kind=dp_t) :: max_hse_error, dpdr, rhog
 
-  integer :: i_conv, i_fluff
+  integer :: i_fluff
 
   type (eos_t) :: eos_state
 
-  namelist /params/ nx, dens_base, temp_base, &
-       low_density_cutoff, dens_conv_zone, M_conv_zone, temp_fluff, &
-       xmin, xmax, &
-       cfrac, prefix
-  
+  real, parameter :: TOL_MASS = 1.e-6_dp_t
+
+  namelist /params/ nx, temp_core, &
+                    low_density_cutoff, M_tot, temp_fluff, &
+                    xmin, xmax, &
+                    hefrac, prefix
+
 
   ! determine if we specified a runtime parameters file or use the default
   narg = command_argument_count()
@@ -99,19 +103,12 @@ program init_1d
   xmin = 0_dp_t
   xmax = 5.d8
 
-  dens_base = 2.6d9
-  temp_base = 6.d8
-     
-  dens_conv_zone = -1.d0
-  M_conv_zone = 2.0d0
-
-
   low_density_cutoff =1.d-4
   temp_fluff = 1.d7
 
-  cfrac = 0.7_dp_t
+  hefrac = 0.3_dp_t
 
-  prefix = "spherical"
+  prefix = "convective"
 
 
   ! check the namelist for any changed parameters
@@ -129,37 +126,30 @@ program init_1d
 
 
   ! get the species indices
-  ic12  = network_species_index("carbon-12")
-  io16  = network_species_index("oxygen-16")
-
-  img24 = network_species_index("magnesium-24")
-  iash = network_species_index("ash")
+  ih1  = network_species_index("hydrogen-1")
+  ihe4  = network_species_index("helium-4")
 
 
-  if (ic12 < 0 .or. io16 < 0) then
+  if (ih1 < 0 .or. ihe4 < 0) then
      call bl_error("ERROR: species not defined")
   endif
 
-  if (.not. (img24 > 0 .or. iash > 0)) then
-     call bl_error("ERROR: species not defined")
-  endif
-
-  if (cfrac < 0.0_dp_t .or. cfrac > 1.0_dp_t) then
+  if (hefrac < 0.0_dp_t .or. hefrac > 1.0_dp_t) then
      call bl_error("ERROR: cfrac must be between 0 and 1")
   endif
 
-  xn_base(:) = 0.0_dp_t
-  xn_base(ic12) = cfrac
-  xn_base(io16) = 1.0_dp_t - cfrac
+  xn_core(:) = 0.0_dp_t
+  xn_core(ihe4) = hefrac
+  xn_core(ih1) = 1.0_dp_t - hefrac
 
-  
+
 
 !-----------------------------------------------------------------------------
 ! Create a 1-d uniform grid that is identical to the mesh that we are
 ! mapping onto, and then we want to force it into HSE on that mesh.
 !-----------------------------------------------------------------------------
 
-! allocate storage
+  ! allocate storage
   allocate(xzn_hse(nx))
   allocate(xznl(nx))
   allocate(xznr(nx))
@@ -168,7 +158,7 @@ program init_1d
   allocate(entropy_want(nx))
   allocate(entropy_store(nx))
 
-! compute the coordinates of the new gridded function
+  ! compute the coordinates of the new gridded function
   dCoord = (xmax - xmin) / dble(nx)
 
   do i = 1, nx
@@ -177,246 +167,278 @@ program init_1d
      xzn_hse(i) = 0.5_dp_t*(xznl(i) + xznr(i))
   enddo
 
+  ! We don't know what central density will give the desired total
+  ! mass, so we need to iterate over central density
 
-  
-  fluff = .false.
+  ! we will do a secant iteration.  rho_c_old is the 'old' guess for
+  ! the central density and rho_c is the current guess.  After 2
+  ! loops, we can start estimating the density required to yield our
+  ! desired mass
+  rho_c_old = -1.0_dp_t
+  rho_c     = 1.e3_dp_t     ! 1.e3 is a reasonable starting low mass star density
 
- 
-
-  ! call the EOS one more time for this zone and then go on to the next
-  eos_state%T     = temp_base
-  eos_state%rho   = dens_base
-  eos_state%xn(:) = xn_base(:)
-
-  ! (t, rho) -> (p, s)    
-  call eos(eos_input_rt, eos_state)
-
-  ! make the initial guess be completely uniform
-  model_hse(:,idens) = eos_state%rho
-  model_hse(:,itemp) = eos_state%T
-  model_hse(:,ipres) = eos_state%p
-
-  do i = 1, nspec
-     model_hse(:,ispec-1+i) = eos_state%xn(i)
-  enddo
-
-  entropy_want(:) = eos_state%s
+  mass_converged = .false.
 
 
-  ! keep track of the mass enclosed below the current zone
-  M_enclosed(1) = FOUR3RD*M_PI*(xznr(1)**3 - xznl(1)**3)*model_hse(1,idens)
+  do iter_mass = 1, MAX_ITER
 
+     print *, 'mass iter = ', iter_mass, rho_c, temp_core
 
-!-----------------------------------------------------------------------------
-! HSE + entropy solve
-!-----------------------------------------------------------------------------
+     fluff = .false.
 
-  isentropic = .true.
+     ! call the EOS one more time to initialize the central conditions.
+     eos_state%T     = temp_core
+     eos_state%rho   = rho_c
+     eos_state%xn(:) = xn_core(:)
 
-  do i = 2, nx
-
-     delx = xzn_hse(i) - xzn_hse(i-1)
-
-     ! as the initial guess for the temperature and density, use the previous
-     ! zone
-     dens_zone = model_hse(i-1,idens)
-     temp_zone = model_hse(i-1,itemp)
-     xn(:) = model_hse(i,ispec:nvar)
-
-     g_zone = -Gconst*M_enclosed(i-1)/(xznl(i)*xznl(i))
-
-
-     !-----------------------------------------------------------------------
-     ! iteration loop
-     !-----------------------------------------------------------------------
-
-     ! start off the Newton loop by saying that the zone has not converged
-     converged_hse = .FALSE.
-
-     if (.not. fluff) then
-
-        do iter = 1, MAX_ITER
-
-           if (isentropic) then
-
-              p_want = model_hse(i-1,ipres) + &
-                   delx*0.5_dp_t*(dens_zone + model_hse(i-1,idens))*g_zone
-         
-           
-              ! now we have two functions to zero:
-              !   A = p_want - p(rho,T)
-              !   B = entropy_want - s(rho,T)
-              ! We use a two dimensional Taylor expansion and find the deltas
-              ! for both density and temperature
-              
-              eos_state%T     = temp_zone
-              eos_state%rho   = dens_zone
-              eos_state%xn(:) = xn(:)
-              
-              ! (t, rho) -> (p, s)    
-              call eos(eos_input_rt, eos_state)
-
-              entropy = eos_state%s
-              pres_zone = eos_state%p
-              
-              dpt = eos_state%dpdt
-              dpd = eos_state%dpdr
-              dst = eos_state%dsdt
-              dsd = eos_state%dsdr
-              
-              A = p_want - pres_zone
-              B = entropy_want(i) - entropy
-              
-              dAdT = -dpt
-              dAdrho = 0.5d0*delx*g_zone - dpd
-              dBdT = -dst
-              dBdrho = -dsd
-              
-              dtemp = (B - (dBdrho/dAdrho)*A)/ &
-                   ((dBdrho/dAdrho)*dAdT - dBdT)
-              
-              drho = -(A + dAdT*dtemp)/dAdrho
-              
-              dens_zone = max(0.9_dp_t*dens_zone, &
-                              min(dens_zone + drho, 1.1_dp_t*dens_zone))
-
-              temp_zone = max(0.9_dp_t*temp_zone, &
-                              min(temp_zone + dtemp, 1.1_dp_t*temp_zone))
-
-
-              ! check if the density falls below our minimum cut-off --
-              ! if so, floor it
-              if (dens_zone < low_density_cutoff) then
-
-                 i_fluff = i
-                 
-                 dens_zone = low_density_cutoff
-                 temp_zone = temp_fluff
-                 converged_hse = .TRUE.
-                 fluff = .TRUE.
-                 exit
-
-              endif
-
-              if (dens_zone < dens_conv_zone .and. isentropic) then
-
-                 i_conv = i                 
-                 isentropic = .false.
-                 
-              endif
-
-
-              ! if (A < TOL .and. B < ETOL) then
-              if (abs(drho) < TOL*dens_zone .and. abs(dtemp) < TOL*temp_zone) then
-                 converged_hse = .TRUE.
-                 exit
-              endif
-       
-           else
-
-              ! do isothermal
-              p_want = model_hse(i-1,ipres) + &
-                   delx*0.5*(dens_zone + model_hse(i-1,idens))*g_zone
-
-              temp_zone = model_hse(i-1,itemp)
-
-
-              eos_state%T     = temp_zone
-              eos_state%rho   = dens_zone
-              eos_state%xn(:) = xn(:)
-        
-              ! (t, rho) -> (p, s)
-              call eos(eos_input_rt, eos_state)
-        
-              entropy = eos_state%s
-              pres_zone = eos_state%p
-              
-              dpd = eos_state%dpdr
-              
-              drho = (p_want - pres_zone)/(dpd - 0.5*delx*g_zone)
-              
-              dens_zone = max(0.9*dens_zone, &
-                   min(dens_zone + drho, 1.1*dens_zone))
-              
-              if (abs(drho) < TOL*dens_zone) then
-                 converged_hse = .TRUE.
-                 exit
-              endif
-
-              
-              if (dens_zone < low_density_cutoff) then
-
-                 i_fluff = i
-                 
-                 dens_zone = low_density_cutoff
-                 temp_zone = temp_fluff
-                 converged_hse = .TRUE.
-                 fluff = .TRUE.
-                 exit
-                 
-              endif
-
-            
-           endif
-
-        enddo
-
-        if (.NOT. converged_hse) then
-           
-           print *, 'Error zone', i, ' did not converge in init_1d'
-           print *, 'integrate up'
-           print *, dens_zone, temp_zone
-           print *, p_want
-           print *, drho
-           call bl_error('Error: HSE non-convergence')
-           
-        endif
-
-        if (temp_zone < temp_fluff) then
-           temp_zone = temp_fluff
-           isentropic = .false.
-        endif
-
-     else
-        dens_zone = low_density_cutoff
-        temp_zone = temp_fluff
-     endif
-
-
-     ! call the EOS one more time for this zone and then go on to the next
-     eos_state%T     = temp_zone
-     eos_state%rho   = dens_zone
-     eos_state%xn(:) = xn(:)
-
-     ! (t, rho) -> (p, s)    
+     ! (t, rho) -> (p, s)
      call eos(eos_input_rt, eos_state)
 
-     pres_zone = eos_state%p
+     ! make the initial guess be completely uniform
+     model_hse(:,idens) = eos_state%rho
+     model_hse(:,itemp) = eos_state%T
+     model_hse(:,ipres) = eos_state%p
 
-     dpd = eos_state%dpdr
-     
-     ! update the thermodynamics in this zone
-     model_hse(i,idens) = dens_zone
-     model_hse(i,itemp) = temp_zone
-     model_hse(i,ipres) = pres_zone
+     do i = 1, nspec
+        model_hse(:,ispec-1+i) = eos_state%xn(i)
+     enddo
 
-     print *, i, dens_zone, temp_zone
-     
-     M_enclosed(i) = M_enclosed(i-1) + &
-          FOUR3RD*M_PI*(xznr(i) - xznl(i))* &
-            (xznr(i)**2 +xznl(i)*xznr(i) + xznl(i)**2)*model_hse(i,idens)
+     entropy_want(:) = eos_state%s
 
-     if (M_enclosed(i) > M_conv_zone*M_sun .and. isentropic) then
+     ! keep track of the mass enclosed below the current zone
+     M_enclosed(1) = FOUR3RD*M_PI*(xznr(1)**3 - xznl(1)**3)*model_hse(1,idens)
 
-        i_conv = i                 
-        isentropic = .false.
-                 
+
+     !-----------------------------------------------------------------------------
+     ! HSE + entropy solve
+     !-----------------------------------------------------------------------------
+
+     isentropic = .true.
+
+     do i = 2, nx
+
+        delx = xzn_hse(i) - xzn_hse(i-1)
+
+        ! as the initial guess for the temperature and density, use the previous
+        ! zone
+        dens_zone = model_hse(i-1,idens)
+        temp_zone = model_hse(i-1,itemp)
+        xn(:) = model_hse(i,ispec:nvar)
+
+        g_zone = -Gconst*M_enclosed(i-1)/(xznl(i)*xznl(i))
+
+        !-----------------------------------------------------------------------
+        ! iteration loop
+        !-----------------------------------------------------------------------
+
+        ! start off the Newton loop by saying that the zone has not converged
+        converged_hse = .FALSE.
+
+        if (.not. fluff) then
+
+           do iter = 1, MAX_ITER
+
+              if (isentropic) then
+
+                 p_want = model_hse(i-1,ipres) + &
+                      delx*0.5_dp_t*(dens_zone + model_hse(i-1,idens))*g_zone
+
+
+                 ! now we have two functions to zero:
+                 !   A = p_want - p(rho,T)
+                 !   B = entropy_want - s(rho,T)
+                 ! We use a two dimensional Taylor expansion and find the deltas
+                 ! for both density and temperature
+
+                 eos_state%T     = temp_zone
+                 eos_state%rho   = dens_zone
+                 eos_state%xn(:) = xn(:)
+
+                 ! (t, rho) -> (p, s)
+                 call eos(eos_input_rt, eos_state)
+
+                 entropy = eos_state%s
+                 pres_zone = eos_state%p
+
+                 dpt = eos_state%dpdt
+                 dpd = eos_state%dpdr
+                 dst = eos_state%dsdt
+                 dsd = eos_state%dsdr
+
+                 A = p_want - pres_zone
+                 B = entropy_want(i) - entropy
+
+                 dAdT = -dpt
+                 dAdrho = 0.5d0*delx*g_zone - dpd
+                 dBdT = -dst
+                 dBdrho = -dsd
+
+                 dtemp = (B - (dBdrho/dAdrho)*A)/ &
+                      ((dBdrho/dAdrho)*dAdT - dBdT)
+
+                 drho = -(A + dAdT*dtemp)/dAdrho
+
+                 dens_zone = max(0.9_dp_t*dens_zone, &
+                      min(dens_zone + drho, 1.1_dp_t*dens_zone))
+
+                 temp_zone = max(0.9_dp_t*temp_zone, &
+                      min(temp_zone + dtemp, 1.1_dp_t*temp_zone))
+
+
+                 ! check if the density falls below our minimum cut-off --
+                 ! if so, floor it
+                 if (dens_zone < low_density_cutoff) then
+
+                    i_fluff = i
+
+                    dens_zone = low_density_cutoff
+                    temp_zone = temp_fluff
+                    converged_hse = .TRUE.
+                    fluff = .TRUE.
+                    exit
+
+                 endif
+
+
+                 ! if (A < TOL .and. B < ETOL) then
+                 if (abs(drho) < TOL*dens_zone .and. abs(dtemp) < TOL*temp_zone) then
+                    converged_hse = .TRUE.
+                    exit
+                 endif
+
+              else
+
+                 ! do isothermal
+                 p_want = model_hse(i-1,ipres) + &
+                      delx*0.5*(dens_zone + model_hse(i-1,idens))*g_zone
+
+                 temp_zone = model_hse(i-1,itemp)
+
+
+                 eos_state%T     = temp_zone
+                 eos_state%rho   = dens_zone
+                 eos_state%xn(:) = xn(:)
+
+                 ! (t, rho) -> (p, s)
+                 call eos(eos_input_rt, eos_state)
+
+                 entropy = eos_state%s
+                 pres_zone = eos_state%p
+
+                 dpd = eos_state%dpdr
+
+                 drho = (p_want - pres_zone)/(dpd - 0.5*delx*g_zone)
+
+                 dens_zone = max(0.9*dens_zone, &
+                      min(dens_zone + drho, 1.1*dens_zone))
+
+                 if (abs(drho) < TOL*dens_zone) then
+                    converged_hse = .TRUE.
+                    exit
+                 endif
+
+
+                 if (dens_zone < low_density_cutoff) then
+
+                    i_fluff = i
+
+                    dens_zone = low_density_cutoff
+                    temp_zone = temp_fluff
+                    converged_hse = .TRUE.
+                    fluff = .TRUE.
+                    exit
+
+                 endif
+
+
+              endif
+
+           enddo
+
+           if (.NOT. converged_hse) then
+
+              print *, 'Error zone', i, ' did not converge in init_1d'
+              print *, 'integrate up'
+              print *, dens_zone, temp_zone
+              print *, p_want
+              print *, drho
+              call bl_error('Error: HSE non-convergence')
+
+           endif
+
+           if (temp_zone < temp_fluff) then
+              temp_zone = temp_fluff
+              isentropic = .false.
+           endif
+
+        else
+           dens_zone = low_density_cutoff
+           temp_zone = temp_fluff
+        endif
+
+
+        ! call the EOS one more time for this zone and then go on to the next
+        eos_state%T     = temp_zone
+        eos_state%rho   = dens_zone
+        eos_state%xn(:) = xn(:)
+
+        ! (t, rho) -> (p, s)
+        call eos(eos_input_rt, eos_state)
+
+        pres_zone = eos_state%p
+
+        ! update the thermodynamics in this zone
+        model_hse(i,idens) = dens_zone
+        model_hse(i,itemp) = temp_zone
+        model_hse(i,ipres) = pres_zone
+
+        !print *, i, dens_zone, temp_zone
+
+        M_enclosed(i) = M_enclosed(i-1) + &
+             FOUR3RD*M_PI*(xznr(i) - xznl(i))* &
+             (xznr(i)**2 +xznl(i)*xznr(i) + xznl(i)**2)*model_hse(i,idens)
+
+     enddo   ! loop over zones
+
+     mass_star = M_enclosed(nx)
+
+     print *, 'mass = ', M_enclosed(nx)/M_sun
+
+     if (rho_c_old < 0.0_dp_t) then
+        ! not enough iterations yet -- store the old central density and
+        ! mass and pick a new value
+        rho_c_old = rho_c
+        mass_star_old = mass_star
+
+        rho_c = 0.5*rho_c_old
+
+     else
+        ! have we converged
+        if ( abs(mass_star - M_tot*M_sun)/(M_tot*M_sun) < TOL_MASS) then
+           mass_converged = .true.
+           exit
+        endif
+
+        ! do a secant iteration:
+        ! M_tot = M(rho_c) + dM/drho |_rho_c x drho + ...        
+        drho_c = (M_tot*M_sun - mass_star)/ &
+             ( (mass_star  - mass_star_old)/(rho_c - rho_c_old) )
+
+        rho_c_old = rho_c
+        mass_star_old = mass_star
+
+        rho_c = min(1.1_dp_t*rho_c_old, &
+             max((rho_c + drho_c), 0.9_dp_t*rho_c_old))
+
      endif
 
-  enddo
+  end do   ! end mass constraint loop
 
+  if (.not. mass_converged) then
+     print *, 'ERROR: star mass did not converge'
+     call bl_error("ERROR: mass did not converge")
+  endif
 
-  print *, 'mass = ', M_enclosed(nx)/M_sun
 
   write(num,'(i8)') nx
   outfile = trim(prefix) // ".hse." // trim(adjustl(num))
@@ -470,11 +492,9 @@ program init_1d
 
 
   print *, 'total mass = ', M_enclosed(i_fluff)/M_sun
-  print *, 'convective zone mass = ', M_enclosed(i_conv)/M_sun
-  
+
 
 
   close (unit=50)
 
 end program init_1d
-
