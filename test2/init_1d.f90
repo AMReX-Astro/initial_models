4df2f94de4a8076febe0638995b8b1b11050ebde
Michael Zingale <michael.zingale@stonybrook.edu>
2020-08-17 19:25:36 -0400

    get this compiling with the new build system

--- test2/init_1d.f90
+++ test2/init_1d.f90
@@ -1,6 +1,6 @@
 !!  Create a 1-d hydrostatic, isoentropic atmosphere given the temperature,
-!!  composition, and base density.  This version allows for an entropy 
-!!  decrease below a height base_height, to make the lower region 
+!!  composition, and base density.  This version allows for an entropy
+!!  decrease below a height base_height, to make the lower region
 !!  convectively stable.
 !!
 !!  The model is placed into HSE by the following differencing:
@@ -14,31 +14,26 @@
 !!   P(i-1) = P_eos(rho(i-1), T(i-1), X(i-1)
 !!
 
-program init_1d
- 
-  use bl_types
-  use bl_constants_module
-  use bl_error_module
+subroutine init_1d() bind(C, name="init_1d")
+
+  use amrex_fort_module, only : rt => amrex_real
+  use amrex_constants_module
+  use amrex_error_module
   use eos_module, only: eos, eos_init
   use eos_type_module, only: eos_input_rt, eos_t
-  use extern_probin_module, only: use_eos_coulomb
+  use extern_probin_module
   use network, only : nspec, network_species_index, spec_names, network_init
 
   implicit none
 
   integer :: i, n
 
-  character(len=128) :: params_file
-      
-  real (kind=dp_t) :: temp_base, dens_base
-  real (kind=dp_t), DIMENSION(nspec) :: xn_base
+  real (kind=rt), DIMENSION(nspec) :: xn_base
 
-  real (kind=dp_t), allocatable :: xzn_hse(:)
-  real (kind=dp_t), allocatable :: model_hse(:,:)
+  real (kind=rt), allocatable :: xzn_hse(:)
+  real (kind=rt), allocatable :: model_hse(:,:)
 
   real :: A, B
-
-  integer :: nx
 
   ! define convenient indices for the scalars
   integer, parameter :: nvar = 3 + nspec
@@ -50,17 +45,15 @@ program init_1d
   ! we'll get the composition from the network module
   integer :: ic12, io16, iash
 
-  real (kind=dp_t) :: xmin, xmax, dCoord
+  real (kind=rt) :: dCoord
 
-  real (kind=dp_t) :: dens_zone, temp_zone, pres_zone, entropy
-  real (kind=dp_t) :: dpd, dpt, dsd, dst
+  real (kind=rt) :: dens_zone, temp_zone, pres_zone, entropy
+  real (kind=rt) :: dpd, dpt, dsd, dst
 
-  real (kind=dp_t) :: p_want, drho, dtemp, delx
-  real (kind=dp_t), allocatable :: entropy_store(:), entropy_want(:)
-  
-  real (kind=dp_t) :: g_zone
+  real (kind=rt) :: p_want, drho, dtemp, delx
+  real (kind=rt), allocatable :: entropy_store(:), entropy_want(:)
 
-  real (kind=dp_t), parameter :: TOL = 1.e-10
+  real (kind=rt), parameter :: TOL = 1.e-10
 
   integer, parameter :: MAX_ITER = 250
 
@@ -68,83 +61,28 @@ program init_1d
 
   logical :: converged_hse, fluff
 
-  real (kind=dp_t), dimension(nspec) :: xn
+  real (kind=rt), dimension(nspec) :: xn
 
-  real (kind=dp_t) :: low_density_cutoff, temp_fluff, smallx
-
-  real (kind=dp_t) :: entropy_jump, entropy_end, base_height
-
-  logical :: do_coulomb
+  real (kind=rt) :: smallx
 
   integer :: index_base
 
-  real (kind=dp_t) :: slope
+  real (kind=rt) :: slope
 
-  logical :: isentropic 
+  logical :: isentropic
 
   character (len=256) :: outfile
   character (len=8) :: num
 
-  real (kind=dp_t) :: max_hse_error, dpdr, rhog
+  real (kind=rt) :: max_hse_error, dpdr, rhog
 
   integer :: narg
 
   type (eos_t) :: eos_state
 
-  namelist /params/ nx, xmin, xmax, base_height, entropy_jump, entropy_end, &
-       dens_base, temp_base, g_zone, low_density_cutoff, temp_fluff, do_coulomb
-
-  ! determine if we specified a runtime parameters file or use the
-  ! default
-  narg = command_argument_count()
-
-  if (narg == 0) then
-     params_file = "_params"
-  else
-     call get_command_argument(1, value = params_file)
-  endif
-
-
-  ! define the defaults parameters for this model
-  nx = 640
-
-  xmin = 0_dp_t
-  xmax = 3.6d8
-
-  base_height = 1.d8
-
-  entropy_jump = 3.d0
-  entropy_end  = 6.d0
-
-  dens_base = 2.6d9
-  temp_base = 7.d8
-
-  g_zone = -1.5d10
-
-  low_density_cutoff = 1.d-4
-  temp_fluff = 1.d7
-
-
-  ! check the namelist for any changed parameters
-  open(unit=11, file=trim(params_file), status="old", action="read")
-  read(unit=11, nml=params)
-  close(unit=11)
-
-
-
-
   ! EOS stuff
   smallx = 1.d-10
 
-  ! this comes in from extern_probin_module -- override here if desired
-  use_eos_coulomb = do_coulomb
-
-
-  ! initialize the EOS and network
-  call eos_init()
-  call network_init()
-
-  
   ! get the species indices
   ic12  = network_species_index("carbon-12")
   io16  = network_species_index("oxygen-16")
@@ -156,13 +94,13 @@ program init_1d
   endif
 
   if (ic12 < 0 .or. io16 < 0 .or. iash < 0) then
-     call bl_error("ERROR: species not defined")
+     call amrex_error("ERROR: species not defined")
   endif
 
-  xn_base(ic12) = 0.3_dp_t
-  xn_base(io16) = 0.7_dp_t
-  xn_base(iash) = 0.0_dp_t
-  
+  xn_base(ic12) = 0.3_rt
+  xn_base(io16) = 0.7_rt
+  xn_base(iash) = 0.0_rt
+
 
 !-----------------------------------------------------------------------------
 ! Create a 1-d uniform grid that is identical to the mesh that we are
@@ -179,13 +117,13 @@ program init_1d
   dCoord = (xmax - xmin) / dble(nx)
 
   do i = 1, nx
-     xzn_hse(i) = xmin + (dble(i) - 0.5_dp_t)*dCoord
+     xzn_hse(i) = xmin + (dble(i) - 0.5_rt)*dCoord
   enddo
 
 
   index_base = -1
 
-! find the index of the base height 
+! find the index of the base height
   do i = 1, nx
      if (xzn_hse(i) >= base_height) then
         index_base = i
@@ -195,7 +133,7 @@ program init_1d
 
   if (index_base == -1) then
      print *, 'ERROR: base_height not found on grid'
-     call bl_error('ERROR: invalid base_height')
+     call amrex_error('ERROR: invalid base_height')
   endif
 
 
@@ -269,7 +207,7 @@ program init_1d
      temp_zone = model_hse(i-1,itemp)
      xn(:) = model_hse(i,ispec:nvar)
 
-     
+
      !-----------------------------------------------------------------------
      ! iteration loop
      !-----------------------------------------------------------------------
@@ -288,11 +226,11 @@ program init_1d
               ! the density of the two zones as an approximation of the
               ! interface value -- this means that we need to iterate for
               ! find the density and pressure that are consistent
-              
+
               ! furthermore, we need to get the entropy that we need,
               ! which will come from adjusting the temperature in
               ! addition to the density.
-        
+
               ! HSE differencing
               p_want = model_hse(i-1,ipres) + &
                    delx*0.5*(dens_zone + model_hse(i-1,idens))*g_zone
@@ -300,12 +238,12 @@ program init_1d
               ! now we have two functions to zero:
               !   A = p_want - p(rho,T)
               !   B = entropy_want - s(rho,T)
-              ! We use a two dimensional Taylor expansion and find the deltas 
+              ! We use a two dimensional Taylor expansion and find the deltas
               ! for both density and temperature
 
-              
-              ! now we know the pressure and the entropy that we want, so we 
-              ! need to find the temperature and density through a two 
+
+              ! now we know the pressure and the entropy that we want, so we
+              ! need to find the temperature and density through a two
               ! dimensional root find
 
               ! (t, rho) -> (p, s)
@@ -314,7 +252,7 @@ program init_1d
               eos_state%xn(:) = xn(:)
 
               call eos(eos_input_rt, eos_state)
-              
+
               entropy = eos_state%s
               pres_zone = eos_state%p
 
@@ -325,43 +263,43 @@ program init_1d
 
               A = p_want - pres_zone
               B = entropy_want(i) - entropy
-              
+
               dtemp = ((dsd/(dpd-0.5*delx*g_zone))*A - B)/ &
                    (dsd*dpt/(dpd -0.5*delx*g_zone) - dst)
-              
+
               drho = (A - dpt*dtemp)/(dpd - 0.5*delx*g_zone)
-              
-              dens_zone = max(0.9_dp_t*dens_zone, &
-                   min(dens_zone + drho, 1.1_dp_t*dens_zone))
 
-              temp_zone = max(0.9_dp_t*temp_zone, &
-                   min(temp_zone + dtemp, 1.1_dp_t*temp_zone))
+              dens_zone = max(0.9_rt*dens_zone, &
+                   min(dens_zone + drho, 1.1_rt*dens_zone))
+
+              temp_zone = max(0.9_rt*temp_zone, &
+                   min(temp_zone + dtemp, 1.1_rt*temp_zone))
 
 
-              ! check if the density falls below our minimum cut-off -- 
+              ! check if the density falls below our minimum cut-off --
               ! if so, floor it
               if (dens_zone < low_density_cutoff) then
-                 
+
                  dens_zone = low_density_cutoff
                  temp_zone = temp_fluff
                  converged_hse = .TRUE.
                  fluff = .TRUE.
                  exit
-              
+
               endif
-                 
+
               ! if (A < TOL .and. B < ETOL) then
               if (abs(drho) < TOL*dens_zone .and. abs(dtemp) < TOL*temp_zone) then
                  converged_hse = .TRUE.
                  exit
               endif
-        
+
            else
 
               ! do isothermal
               p_want = model_hse(i-1,ipres) + &
                    delx*0.5*(dens_zone + model_hse(i-1,idens))*g_zone
-         
+
               temp_zone = temp_fluff
 
               ! (t, rho) -> (p)
@@ -370,45 +308,45 @@ program init_1d
               eos_state%xn(:) = xn(:)
 
               call eos(eos_input_rt, eos_state)
-              
+
               entropy = eos_state%s
               pres_zone = eos_state%p
 
               dpd = eos_state%dpdr
 
               drho = (p_want - pres_zone)/(dpd - 0.5*delx*g_zone)
-              
+
               dens_zone = max(0.9*dens_zone, &
                    min(dens_zone + drho, 1.1*dens_zone))
-       
+
               if (abs(drho) < TOL*dens_zone) then
                  converged_hse = .TRUE.
                  exit
               endif
 
               if (dens_zone < low_density_cutoff) then
-                 
+
                  dens_zone = low_density_cutoff
                  temp_zone = temp_fluff
                  converged_hse = .TRUE.
                  fluff = .TRUE.
                  exit
-              
+
               endif
 
            endif
 
         enddo
-        
+
         if (.NOT. converged_hse) then
-           
+
            print *, 'Error zone', i, ' did not converge in init_1d'
            print *, 'integrate up'
            print *, dens_zone, temp_zone
            print *, p_want, entropy_want(i), entropy
            print *, drho, dtemp
-           call bl_error('Error: HSE non-convergence')
-           
+           call amrex_error('Error: HSE non-convergence')
+
         endif
 
         if (temp_zone < temp_fluff) then
@@ -431,7 +369,7 @@ program init_1d
      call eos(eos_input_rt, eos_state)
 
      pres_zone = eos_state%p
-     
+
      ! update the thermodynamics in this zone
      model_hse(i,idens) = dens_zone
      model_hse(i,itemp) = temp_zone
@@ -473,11 +411,11 @@ program init_1d
         ! the density of the two zones as an approximation of the
         ! interface value -- this means that we need to iterate for
         ! find the density and pressure that are consistent
-        
+
         ! furthermore, we need to get the entropy that we need,
         ! which will come from adjusting the temperature in
         ! addition to the density.
-        
+
         ! HSE differencing
         p_want = model_hse(i+1,ipres) - &
              delx*0.5*(dens_zone + model_hse(i+1,idens))*g_zone
@@ -485,12 +423,12 @@ program init_1d
         ! now we have two functions to zero:
         !   A = p_want - p(rho,T)
         !   B = entropy_want - s(rho,T)
-        ! We use a two dimensional Taylor expansion and find the deltas 
+        ! We use a two dimensional Taylor expansion and find the deltas
         ! for both density and temperature
-        
-        
-        ! now we know the pressure and the entropy that we want, so we 
-        ! need to find the temperature and density through a two 
+
+
+        ! now we know the pressure and the entropy that we want, so we
+        ! need to find the temperature and density through a two
         ! dimensional root find
         ! (t, rho) -> (p, s)
         eos_state%T     = temp_zone
@@ -498,47 +436,47 @@ program init_1d
         eos_state%xn(:) = xn(:)
 
         call eos(eos_input_rt, eos_state)
-        
+
         entropy = eos_state%s
         pres_zone = eos_state%p
-        
+
         dpt = eos_state%dpdt
         dpd = eos_state%dpdr
         dst = eos_state%dsdt
         dsd = eos_state%dsdr
-              
+
         A = p_want - pres_zone
         B = entropy_want(i) - entropy
-              
+
         dtemp = ((dsd/(dpd+0.5*delx*g_zone))*A - B)/ &
              (dsd*dpt/(dpd +0.5*delx*g_zone) - dst)
-        
-        drho = (A - dpt*dtemp)/(dpd + 0.5*delx*g_zone)
-        
-        dens_zone = max(0.9_dp_t*dens_zone, &
-             min(dens_zone + drho, 1.1_dp_t*dens_zone))
 
-        temp_zone = max(0.9_dp_t*temp_zone, &
-             min(temp_zone + dtemp, 1.1_dp_t*temp_zone))
-        
-                        
+        drho = (A - dpt*dtemp)/(dpd + 0.5*delx*g_zone)
+
+        dens_zone = max(0.9_rt*dens_zone, &
+             min(dens_zone + drho, 1.1_rt*dens_zone))
+
+        temp_zone = max(0.9_rt*temp_zone, &
+             min(temp_zone + dtemp, 1.1_rt*temp_zone))
+
+
         if (abs(drho) < TOL*dens_zone .and. abs(dtemp) < TOL*temp_zone) then
            converged_hse = .TRUE.
            exit
         endif
-        
+
 
      enddo
-        
+
      if (.NOT. converged_hse) then
-        
+
         print *, 'Error zone', i, ' did not converge in init_1d'
         print *, 'integrate down'
         print *, dens_zone, temp_zone
         print *, p_want, entropy_want(i), entropy
         print *, drho, dtemp
-        call bl_error('Error: HSE non-convergence')
-           
+        call amrex_error('Error: HSE non-convergence')
+
      endif
 
 
@@ -551,7 +489,7 @@ program init_1d
      call eos(eos_input_rt, eos_state)
 
      pres_zone = eos_state%p
-     
+
      ! update the thermodynamics in this zone
      model_hse(i,idens) = dens_zone
      model_hse(i,itemp) = temp_zone
@@ -605,10 +543,4 @@ program init_1d
 
   close (unit=50)
 
-end program init_1d
-
-
-
-
-
-
+end subroutine init_1d
