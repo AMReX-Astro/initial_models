0fe171e4abe2c9a7918fc179eb2a94c88e40d041
Michael Zingale <michael.zingale@stonybrook.edu>
2021-06-29 09:35:12 -0400

    add doc building action

---
+++ deploy_docs_action.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e # Exit with nonzero exit code if anything fails
+
+# Build the documentation from the MAIN_BRANCH or DEV_BRANCH
+# and push it to TARGET_BRANCH.
+MAIN_BRANCH="main"
+TARGET_BRANCH="gh-pages"
+
+mkdir out
+
+# Build the Sphinx documentation
+cd Docs
+make html
+cd ../
+
+mkdir -p out/docs/
+if [ "$GITHUB_BRANCH" = "$MAIN_BRANCH" ]; then
+    mkdir -p out/docs
+    mv sphinx_docs/build/html/* out/docs
+fi
